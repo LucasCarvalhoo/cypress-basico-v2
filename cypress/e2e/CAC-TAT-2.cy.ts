0475a096ca68848319e8b4b094b722c273094d5d
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-08-06 08:29:22 -0300

    test: novos testes

---
+++ cypress/e2e/CAC-TAT-2.cy.ts
@@ -0,0 +1,8 @@
+// <reference types="Cypress" />
+
+// describe define o suite de teste, e o bloco it, define um caso de teste.
+describe('Central de Atendimento ao Cliente TAT', function() {
+  beforeEach(() => {
+    cy.visit('../../src/index.html')
+  })
+})
